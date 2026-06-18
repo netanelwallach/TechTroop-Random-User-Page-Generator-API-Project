dcc17da759184cc4e53c202ab5e1e684bd5ded14
Netanel Wallach <netanelwallach@gmail.com>
2026-06-18 23:04:31 +0300

    user controller handke main user section

---
+++ controller/userController.js
@@ -0,0 +1,18 @@
+import { getUsers } from "../services/user.js";
+
+export async function initUserController() {
+  try {
+    const userData = await getUsers();
+    if (!userData) return;
+
+    const { mainUser, friends } = userData;
+
+    document.getElementById("main-user-picture").src = mainUser.picture;
+    document.getElementById("main-user-fullname").textContent =
+      `${mainUser.firstName} ${mainUser.lastName}`;
+    document.getElementById("main-user-location").textContent =
+      `${mainUser.city}, ${mainUser.state}`;
+  } catch (error) {
+    console.error("Controller Error:", error);
+  }
+}
