3868b96014bb5b61e9826eacf2261d548ee25f01
Netanel Wallach <netanelwallach@gmail.com>
2026-06-18 23:31:02 +0300

    refactor code to keep seperation of handling

--- controller/userController.js
+++ controller/userController.js
@@ -7,15 +7,35 @@ export async function initUserController() {
 
     const { mainUser, friends } = userData;
 
-    document.getElementById("main-user-picture").src = mainUser.picture;
-    document.getElementById("main-user-picture").alt = mainUser.firstName;
+    initMainUser(mainUser);
 
-    document.getElementById("main-user-fullname").textContent =
-      `${mainUser.firstName} ${mainUser.lastName}`;
-
-    document.getElementById("main-user-location").textContent =
-      `${mainUser.city}, ${mainUser.state}`;
+    initFriends(friends);
   } catch (error) {
     console.error("Controller Error:", error);
   }
+}
+
+function initMainUser(mainUser) {
+  document.getElementById("main-user-picture").src = mainUser.picture;
+  document.getElementById("main-user-picture").alt = mainUser.firstName;
+
+  document.getElementById("main-user-fullname").textContent =
+    `${mainUser.firstName} ${mainUser.lastName}`;
+
+  document.getElementById("main-user-location").textContent =
+    `${mainUser.city}, ${mainUser.state}`;
+}
+
+function initFriends(friends) {
+  const friendsContainer = document.getElementById("friends-container");
+  friendsContainer.innerHTML = friends
+    .map(
+      (friend) => `
+      <li>
+        <img src="${friend.picture}" alt="${friend.firstName} ${friend.lastName}">
+        <span>${friend.firstName} ${friend.lastName}</span>
+      </li>
+    `,
+    )
+    .join("");
 }
