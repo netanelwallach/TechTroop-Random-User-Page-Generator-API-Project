85866c6bc53e6f2f0c17e5b5b6615f59be394165
Netanel Wallach <netanelwallach@gmail.com>
2026-06-19 19:34:26 +0300

    add quote init to app and make pokemon more understood with a header

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { initUserController } from "./controller/userController.js";
 import { initAboutMeController } from "./controller/aboutMeController.js";
 import { initPokemonController } from "./controller/pokemonController.js";
+import { initQuoteController } from "./controller/quoteController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initUserController();
   initAboutMeController();
   initPokemonController();
+  initQuoteController();
 });
