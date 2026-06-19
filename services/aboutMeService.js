cc133c49e2330e2da2285d97ec20e1db3ff57c61
Netanel Wallach <netanelwallach@gmail.com>
2026-06-19 17:48:28 +0300

    corrections for aboutMeService for getting a descent response

--- services/aboutMeService.js
+++ services/aboutMeService.js
@@ -1,14 +1,15 @@
 const ABOUT_ME_API_URL =
-  "https://baconipsum.com/api/?type=meat-and-filler&paras=2&format=text";
+  "https://baconipsum.com/api/?type=meat-and-filler&paras=2&format=json";
 
 export async function getAboutMe() {
   try {
     const aboutMeResponse = await fetch(ABOUT_ME_API_URL);
     if (!aboutMeResponse.ok) {
-      throw new Error("failed creating about me");
+      throw new Error("failed creating About Me");
     }
 
-    const aboutMe = await aboutMeResponse.json();
+    let aboutMe = await aboutMeResponse.json();
+    aboutMe = aboutMe.join("\n\n");
     console.log(aboutMe);
 
     return aboutMe;
